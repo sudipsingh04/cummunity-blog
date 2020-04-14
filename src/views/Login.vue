3b2b09a6eaec6e8bb585a8922520316213de3965
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-14 14:43:48 +0530

    login signup form created

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>LOGIN COMPONENT</h1>
+    <div class="row my-5">
+        <div class="col-md-6 offset-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="text-center my-4">Login</h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="form-group text-center">
+                        <button class="form-control btn btn-success">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>SIGNUP COMPONENT</h1>
+    <div class="row my-5">
+        <div class="col-md-6 offset-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="text-center my-4">Signup</h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password">
+                    </div>
+                    <div class="form-group text-center">
+                        <button class="form-control btn btn-success">Signup</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
