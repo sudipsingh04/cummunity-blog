52312068899a41d03d8db75f7f09e88174518096
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-15 15:24:29 +0530

    cloudinary image upload

--- src/views/CreateArticle.vue
+++ src/views/CreateArticle.vue
@@ -4,10 +4,16 @@
             <div class="col-md-8 offset-md-2">
                 <div class="card my-5">
                     <div class="card-body">
+                        <picture-input 
+                        accept="image/jpeg,image/png" 
+                        size="5" 
+                        button-class="btn btn-danger"
+                        @change="onChange">
+                        </picture-input>
                         <input type="text" placeholder="Title" class="form-control mb-3">
                         <wysiwyg v-model="content" />
                         <div class="text-center">
-                            <button class="btn-success btn-lg mt-3">Create Article</button>
+                            <button @click="createArticle" class="btn-success btn-lg mt-3">Create Article</button>
                         </div>
                     </div>
                 </div>
@@ -17,10 +23,34 @@
 </template>
 
 <script>
+import PictureInput from 'vue-picture-input'
+import Axios from 'axios'
+
 export default {
+    components: {
+        PictureInput
+    },
+
     data(){
         return{
-            content: ""
+            content: "",
+            image: null,
+        }
+    },
+
+    methods: {
+        onChange(image){
+            this.image = image
+        },
+
+        createArticle(){
+            const form = new FormData();
+            form.append('file', this.image)
+            form.append('upload_preset', 'ml_default')
+            form.append('api_key', '159176248862493')
+
+            Axios.post('https://api.cloudinary.com/v1_1/decjesy9a/image/upload', form)
+            .then(res => console.log(res))
         }
     }
 }
