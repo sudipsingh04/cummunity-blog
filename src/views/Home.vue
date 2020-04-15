59aab8c3b60963c35c7687e84daabcb67ffc57b5
sudipsingh04 <sudipsingh04@gmail.com>
2020-04-15 13:12:12 +0530

    added pagination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="row" v-if="articles.data">
-        <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
-            <Article :article="article" />
+    <div>
+        <div class="d-flex justify-content-between mt-4">
+            <button @click="getPrevArticles" :disabled="articles.prev_page_url === null" class="btn btn-warning">Prev Page</button>
+            <button @click="getNextArticles" :disabled="articles.next_page_url === null" class="btn btn-warning">Next Page</button>
+        </div>
+        <div class="row" v-if="!loading">
+            <div class="col-md-8 offset-md-2" v-for="article in articles.data" :key="article.id">
+                <Article :article="article" />
+            </div>
+        </div>
+        <div class="loader text-center" v-else>
+            <i class="fa fa-3x fa-spin fa-spinner"/>
         </div>
     </div>
 </template>
@@ -22,17 +31,34 @@ export default {
 
     data() {
         return{
-           articles: {} 
+           articles: {},
+           loading: true 
         }
     },
 
     methods: {
-        getArticles(){
-            Axios.get(`${config.apiUrl}/articles`).then(response => {
+        getArticles(url = `${config.apiUrl}/articles`){
+            this.loading = true
+            Axios.get(url).then(response => {
+                this.loading = false
                 this.articles = response.data.data;
                 
             })
+        },
+
+        getNextArticles(){
+            this.getArticles(this.articles.next_page_url)
+        },
+
+        getPrevArticles(){
+            this.getArticles(this.articles.prev_page_url)
         }
     }
 }
 </script>
+
+<style>
+.btn-warning{
+    color: #ffffff;
+}
+</style>
